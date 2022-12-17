7ceaa664d48a0e3a9e01f2bbfa795f55fc486312
satijakritik <55873875+satijakritik@users.noreply.github.com>
2022-12-17 18:47:11 +0800

    Update market-simulator.py
     Updated to write a given number of snapshots to orderbook.json. Also added ability to give command line arguments

--- market-simulator.py
+++ market-simulator.py
@@ -1,32 +1,53 @@
 import asyncio
+import ujson
+import time
+import sys
 
 from gate_ws import Configuration, Connection, WebSocketResponse
 from gate_ws.spot import SpotPublicTradeChannel, SpotOrderBookChannel, SpotOrderBookUpdateChannel
 
+NUM_OF_SNAPSHOTS = 5
+curr_snapshot = 0
+orders = []
 
 # define your callback function on message received
 def print_message(conn: Connection, response: WebSocketResponse):
+    global curr_snapshot
+    global NUM_OF_SNAPSHOTS
+    global orders
     if response.error:
         print('error returned: ', response.error)
         conn.close()
         return
-    print(response.result)
-
-
+    
+    data = response.result
+    orders.append(data)
+    curr_snapshot += 1
+    if curr_snapshot >= NUM_OF_SNAPSHOTS:
+        conn.close()
+        return
+        
 async def main():
-    # initialize default connection, which connects to spot WebSocket V4
+    channel_name = sys.argv[1] if len(sys.argv) > 1 else "BTC_USD"
+    depth = sys.argv[2] if len(sys.argv) > 2 else "20"
+    update_rate = sys.argv[3] if len(sys.argv) > 3 else "1000ms"
+    
+    filename = "orderbook.json"
+    #initialize default connection, which connects to spot WebSocket V4
     # it is recommended to use one conn to initialize multiple channels
     conn = Connection(Configuration())
 
     # subscribe to any channel you are interested into, with the callback function
     channel = SpotOrderBookChannel(conn, print_message)
-    channel.subscribe(["BTC_USDT", "20", "1000ms"])
+    channel.subscribe([channel_name, depth, update_rate])
 
     # start the client
     await conn.run()
-
+    
+    with open(filename, "w") as file:
+        data = ujson.dumps(orders, indent=4)
+        print(data)
+        file.write(data)
 
 if __name__ == '__main__':
-   loop = asyncio.get_event_loop()
-   loop.run_until_complete(main())
-   loop.close()
+    asyncio.run(main())
